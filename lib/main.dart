00dd29f362bbee1eda8808a8c330d42f5329432c
mathrews <mateusps4gg@gmail.com>
2024-07-09 13:55:13 -0300

    adding some functions on UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -19,12 +20,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // widget "inicial"
     return ChangeNotifierProvider(
-      create: (context) => CoolNameState(),
+      create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Cool Names',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromRGBO(206, 61, 243, 1.0)),
         ),
         home: MyHomePage(),
       ),
@@ -39,57 +41,152 @@ class MyApp extends StatelessWidget {
 // emitir notificações sobre suas próprias mudanças. Se o par de palavras atual mudar,
 // alguns widgets deverão saber disso.
 // O estado é criado e fornecido a todo o app usando um ChangeNotifierProvider.
-class CoolNameState extends ChangeNotifier {
+class MyAppState extends ChangeNotifier {
   // define o estado do app.
   var current = WordPair.random();
+
+  var pressedState = false;
+
+  get first => null;
+
+  get second => null;
 
   // O novo método getNext() reatribui o widget current a um novo WordPair aleatório.
   void getNext() {
     current = WordPair.random();
-    // envia uma notificação a qualquer elemento que esteja observando MyAppState. 
+    pressedState = false;
+    // envia uma notificação a qualquer elemento que esteja observando MyAppState.
+    notifyListeners();
+  }
+
+  var favorites = Set.from(<WordPair>{});
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      pressedState = false;
+      favorites.remove(current);
+      print(favorites);
+    } else {
+      pressedState = true;
+      favorites.add(current);
+      print(favorites);
+    }
     notifyListeners();
   }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
-  // Cada widget define um método que é chamado sempre que as circunstâncias
-  // do widget mudam, para que ele sempre fique atualizado. Cada método build
-  // precisa retornar um widget ou uma árvore aninhada de widgets.
   Widget build(BuildContext context) {
-    // Rastreia mudanças no estado atual do app utilizando o médodo watch.
-    var appState = context.watch<CoolNameState>();
-
-    // Widget de nível superior. Widget útil muito encontrado
-    // na maioria dos apps do mundo real. Utilizado para definir o esqueleto
-    // padrão dos apps android.
     return Scaffold(
-      // Column é um widget padrão do flutter que recebe qualquer número de filhos
-      // e os coloca em uma coluna de cima para baixo. Por padrão, a coluna coloca
-      // os filhos visualmente no topo. Cria uma col dentro do scaffold.
-      body: Column(
+      body: Row(
         children: [
-          // Widget que define um texto
-          Text('A random cool idea:'),
-          // Usa a var appState que está acessando a classe CoolNameState pegando
-          // o único elemento ("current"). O WordPair fornece vários métodos como
-          // asLowerCase, entre outros.
-          Text(appState.current.asUpperCase),
-          ElevatedButton(
-              onPressed: () {
-                appState.getNext();
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
               },
-              child: Text('next')),
-          ElevatedButton(
-              onPressed: () {
-                print("Favorite word");
-              },
-              child: Icon(Icons.heart_broken))
-          // Essa vírgula específica não precisa estar ali, porque children
-          // é o último (e também o único) membro dessa lista de parâmetros Column
-          // No entanto, essa virgula eh necessaria para o formatador funcionar com
-          // integridade.
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text(appState.pressedState ? 'Remove' : 'Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// classe gerada quando usamos a função de refatoração do VScode
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displayMedium?.copyWith(
+      color: theme.colorScheme.surface,
+      fontWeight: FontWeight.bold,
+    );
+
+    // O padding não é um atributo de Textm e sim um widget.
+    // Os widgets podem focar em uma única responsabilidade, possibilitando assim
+    // total liberdade para criar a interface.
+    return Card(
+      color: theme.colorScheme.primary,
+      elevation: 16.0,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
